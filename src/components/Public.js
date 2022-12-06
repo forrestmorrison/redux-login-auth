e507a1b6ad84bed05aa00b2128d51d6d3cf2926f
forrestmorrison <forrestmorrison@gmail.com>
2022-12-05 20:11:05 -0600

    fixed typos, updated CSS

--- src/components/Public.js
+++ src/components/Public.js
@@ -4,20 +4,10 @@ const Public = () => {
     const content = (
         <section className="public">
             <header>
-                <h1>Welcome to Repair Store!</h1>
+                <h1>POSTS</h1>
             </header>
-            <main>
-                <p>Located in beautiful downtown Foo City</p>
-                <p>&nbsp;</p>
-                <address>
-                    Repair Store<br />
-                    555 Foo Drive<br />
-                    Foo City, CA 12345<br />
-                    <a href="tel:+15555555555">(555) 555-5555</a>
-                </address>
-            </main>
             <footer>
-                <Link to="/login">Employee Login</Link>
+                <Link to="/login">Login</Link>
             </footer>
         </section>
     )
